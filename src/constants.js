5bc410a57b458a0c419d6d590d1e094bad1e7818
Tina Wu <tinawu0603@hotmail.com>
2020-01-31 21:55:03 -0500

    added constants file for all constants in project

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const API_URL = 'https://wbdv-generic-server.herokuapp.com/api/001280266/courses'
+export const DEFAULT_COURSE_TITLE = "New Course"
+export const DEFAULT_SECTION_TITLE = "New Section"
