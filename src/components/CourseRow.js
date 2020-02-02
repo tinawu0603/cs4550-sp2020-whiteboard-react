53e8dbce2351cf16831bbe94ff8eafc9c98a4782
Tina Wu <tinawu0603@hotmail.com>
2020-02-02 18:00:05 -0500

    added template files from class

---
+++ src/components/CourseRow.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CourseRow extends React.Component {
+    state = {
+        editing: false
+    }
+
+    render() {
+        return (
+            <h1>Course Row</h1>
+        )
+    }
+}
